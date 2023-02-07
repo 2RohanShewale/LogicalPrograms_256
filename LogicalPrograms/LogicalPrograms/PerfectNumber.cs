4216b7781afe9e245140ff032023def715608957
Rohan Shewale <shewalerohan50@gmail.com>
2023-02-07 13:48:05 +0530

    [Rohan]UC2: Perfect Numbers

---
+++ LogicalPrograms/LogicalPrograms/PerfectNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class PerfectNumber
+    {
+        int sum = 0;
+        public void Number()
+        {
+            Console.WriteLine("\n*_____Perfect Number_____*");
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= number/2 ; i++)
+            {
+                if (number % i == 0)
+                    sum += i;
+            }
+            if (sum == number)
+                Console.WriteLine($">>>>{number} is perfect number");
+            else
+                Console.WriteLine($">>>>{number} is not perfect number");
+
+            Program.Main();
+
+        }
+    }
+}
--- LogicalPrograms/LogicalPrograms/Program.cs
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -11,7 +11,7 @@ namespace LogicalPrograms
         public static void Main()
         {
             Console.WriteLine("*________Logical Programs________*");
-            Console.WriteLine("1. Fibonacci series");
+            Console.WriteLine("1. Fibonacci series\n2. Perfect Number");
             Console.Write("Enter: ");
             int option = Convert.ToInt32(Console.ReadLine());
 
@@ -20,6 +20,10 @@ namespace LogicalPrograms
                 case 1:
                     FibonacciSeries fibonacci = new FibonacciSeries();
                     fibonacci.Series();
+                    break;
+                case 2:
+                    PerfectNumber perfect = new PerfectNumber();
+                    perfect.Number(); 
                     break;
                 default:Main();
                     break;
